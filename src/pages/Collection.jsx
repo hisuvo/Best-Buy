d1a24674a4037f535cb219de6564ebf1f2a1abd3
suvo <suvodatta72@gamil.com>
2025-09-11 11:18:36 +0600

    collection page inside sort and category filter logic added done

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import ShopContext from "../context/ShopContext";
 import { assets } from "../assets/assets";
 import Title from "../components/common/Title";
@@ -7,6 +7,77 @@ import ProductItem from "../components/ProductItem";
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("relavent");
+
+  const toogleChange = (e) => {
+    if (category.includes(e.target.value)) {
+      // If the value is already selected, remove it
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      // If the value is not selected, add it
+      setCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const toogleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)) {
+      // if the value is already selected then remove it
+      setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      // if the value is not selected then add it
+      setSubCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  let productsCopy = products.slice();
+
+  const applyFilter = () => {
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category)
+      );
+    }
+
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
+    }
+    setFilterProducts(productsCopy);
+  };
+
+  const sortProducts = () => {
+    let sortProductsCopy = products.slice();
+
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(sortProductsCopy.sort((a, b) => a.price - b.price));
+        break;
+
+      case "high-low":
+        setFilterProducts(sortProductsCopy.sort((a, b) => b.price - a.price));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  };
+
+  // useEffect(() => {
+  //   setFilterProducts(products);
+  // }, [products]);
+
+  useEffect(() => {
+    applyFilter();
+  }, [category, subCategory]);
+
+  useEffect(() => {
+    sortProducts();
+  }, [sortType]);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 border-t pt-10">
@@ -33,21 +104,21 @@ const Collection = () => {
           <p className="font-medium mb-3 text-sm">CATEGORIES</p>
           <ul className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <li className="flex gap-2">
-              <input type="checkbox" name="" value={"Men"} />
+              <input type="checkbox" value={"Men"} onChange={toogleChange} />
               Men
             </li>
             <li className="flex gap-2">
-              <input type="checkbox" name="" value={"Women"} />
+              <input type="checkbox" value={"Women"} onChange={toogleChange} />
               Women
             </li>
             <li className="flex gap-2">
-              <input type="checkbox" name="" value={"Kids"} />
+              <input type="checkbox" value={"Kids"} onChange={toogleChange} />
               Kids
             </li>
           </ul>
         </div>
 
-        {/* Type Filter */}
+        {/* subCategory Filter */}
         <div
           className={`border border-gray-300 pl-5 py-3 mt-6 ${
             showFilter ? "" : "hidden"
@@ -56,15 +127,27 @@ const Collection = () => {
           <p className="font-medium mb-3 text-sm">TYPE</p>
           <ul className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <li className="flex gap-2">
-              <input type="checkbox" name="" value={"Topwear"} />
+              <input
+                type="checkbox"
+                value={"Topwear"}
+                onChange={toogleSubCategory}
+              />
               Topwear
             </li>
             <li className="flex gap-2">
-              <input type="checkbox" name="" value={"Bottomwear"} />
+              <input
+                type="checkbox"
+                value={"Bottomwear"}
+                onChange={toogleSubCategory}
+              />
               Bottomwear
             </li>
             <li className="flex gap-2">
-              <input type="checkbox" name="" value={"WinterWear"} />
+              <input
+                type="checkbox"
+                value={"WinterWear"}
+                onChange={toogleSubCategory}
+              />
               Winterwear
             </li>
           </ul>
@@ -77,25 +160,25 @@ const Collection = () => {
           <Title text1={"ALL"} text2={"COLLECTIONS"} />
           <div>
             <select
-              name=""
+              onChange={(e) => setSortType(e.target.value)}
               className="px-2 text-sm sm:text-base py-2 border-2 border-gray-300 outline-none"
             >
-              <option>Sort by: Relavent</option>
-              <option>Sort by: Low to High</option>
-              <option>Sort by: High to Low</option>
+              <option value={"relavent"}>Sort by: Relavent</option>
+              <option value={"low-high"}>Sort by: Low to High</option>
+              <option value={"high-low"}>Sort by: High to Low</option>
             </select>
           </div>
         </div>
 
         {/* All Products */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 gap-y-6">
-          {products.map((item) => (
+          {filterProducts.map((item) => (
             <ProductItem
-              key={item._id}
-              id={item._id}
-              img={item.image[0]}
-              name={item.name}
-              price={item.price}
+              key={item?._id}
+              id={item?._id}
+              img={item?.image[0]}
+              name={item?.name}
+              price={item?.price}
             />
           ))}
         </div>
