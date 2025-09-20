ff58c88deb84bdb6c03b21ee547c2fde360425ff
suvo <suvodatta72@gamil.com>
2025-09-20 23:22:40 +0600

    about and contact page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,77 @@
-import React from "react";
 import NewsLetterBox from "../components/common/NewsLetterBox";
+import { assets } from "../assets/assets";
+import Title from "../components/common/Title";
 
 const About = () => {
   return (
     <div className="border-t pt-14">
+      <div className="text-center text-2xl">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      {/* --------------- about image and description --------------- */}
+      <div className="flex flex-col sm:flex-row gap-16 my-10">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt="About section image"
+        />
+        <div className="flex flex-col gap-6 justify-center md:w-2/4 text-gray-600">
+          <p>
+            Forever was born out of a passion for innovation and a desire to
+            revolutionize the way people shop online. Our journey began with a
+            simple idea: to provide a platform where customers can easily
+            discover, explore, and purchase a wide range of products from the
+            comfort of their homes.
+          </p>
+          <p>
+            Since our inception, we've worked tirelessly to curate a diverse
+            selection of high-quality products that cater to every taste and
+            preference. From fashion and beauty to electronics and home
+            essentials, we offer an extensive collection sourced from trusted
+            brands and suppliers.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Our mission at Forever is to empower customers with choice,
+            convenience, and confidence. We're dedicated to providing a seamless
+            shopping experience that exceeds expectations, from browsing and
+            ordering to delivery and beyond.
+          </p>
+        </div>
+      </div>
+
+      {/* ----------------- why choose section --------------------- */}
+      <div className="mb-10">
+        <div className="text-2xl">
+          <Title text1={"WHY"} text2={"CHOOSE US"} />
+        </div>
+
+        <div className="flex flex-col md:flex-row text-sm mt-6">
+          <div className="border border-gray-200 px-10 md:px-16 py-8 md:py-20 flex flex-col gap-5">
+            <b>Quality Assurance:</b>
+            <p>
+              We meticulously select and vet each product to ensure it meets our
+              stringent quality standards.
+            </p>
+          </div>
+
+          <div className="border border-gray-200 px-10 md:px-16 py-8 md:py-20 flex flex-col gap-5">
+            <b>Quality Assurance:</b>
+            <p>
+              We meticulously select and vet each product to ensure it meets our
+              stringent quality standards.
+            </p>
+          </div>
+          <div className="border border-gray-200 px-10 md:px-16 py-8 md:py-20 flex flex-col gap-5">
+            <b>Quality Assurance:</b>
+            <p>
+              We meticulously select and vet each product to ensure it meets our
+              stringent quality standards.
+            </p>
+          </div>
+        </div>
+      </div>
+
       <NewsLetterBox />
     </div>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import NewsLetterBox from "../components/common/NewsLetterBox";
+import { assets } from "../assets/assets";
+import Title from "../components/common/Title";
+import Button from "../components/common/Button";
 
 const Contact = () => {
   return (
     <div className="border-t pt-14">
+      <div className="text-center text-2xl">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+      {/* --------------- contact image and information --------------- */}
+      <div className="flex flex-col sm:flex-row gap-16 mt-10 mb-20">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.contact_img}
+          alt="About section image"
+        />
+        <div className="flex flex-col gap-6 justify-center md:w-2/4 text-gray-600">
+          <b className="text-xl">Our Store</b>
+          <div className="">
+            <p>54709 Willms Station</p>
+            <p>Suite 350, Washington, USA</p>
+          </div>
+          <div className="">
+            <p>Tel: (415) 555-0132</p>
+            <p>Email: admin@forever.com</p>
+          </div>
+          <b className=" text-xl">Careers at Forever</b>
+          <p>Learn more about our teams and job openings.</p>
+
+          <Button>Explore Jobs</Button>
+        </div>
+      </div>
       <NewsLetterBox />
     </div>
   );
